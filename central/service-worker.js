6ec80314ce326f19fa0e939176c2d7e0501bf99b
izaeltec-sys <311485366+izaeltec-sys@users.noreply.github.com>
2026-09-01 15:15:18 -0400

    chore: incluir sprite de ícones no cache da Central

--- central/service-worker.js
+++ central/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='portal-central-v3';
-const CORE=['./','./index.html','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.svg'];
+const CACHE='portal-central-v4';
+const CORE=['./','./index.html','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.svg','./assets/icons/tabler/sprite.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
